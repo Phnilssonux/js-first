a60ff2bcb967b317a9b3f767a01ed1c6a43509db
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2024-10-15 18:39:34 +0200

    final commit

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(`Hello World!`);
+
+let firstName = "Philip";
+let age = 31;
+let favoriteColor = "Green"
+
+console.log(`My name is ${firstName} I'm ${age} years old and I like the color ${favoriteColor}`);
+
+
+let cookies = 10;
+let kids = 5;
+
+cookies +=10
+kids *= 2;
+kids -= 2;
+
+console.log(cookies/kids)
